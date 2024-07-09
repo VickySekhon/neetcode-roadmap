fe251336dd3e008f7e73d2bae9bbdabce3375c27
Vicky Sekhon <114193273+VickySekhon@users.noreply.github.com>
2024-07-09 13:33:51 -0400

    Finished medium group-anagrams

---
+++ arrays-and-hashing/group-anagrams.py
@@ -0,0 +1,27 @@
+def group_anagrams(words):
+     """
+     Groups a list of words into anagrams.
+
+     Parameters:
+     words (list): A list of strings representing words.
+
+     Returns:
+     list: A list of lists, where each inner list contains words that are anagrams of each other.
+     """
+     hashMap = {}
+     result = []
+
+     for i in words:
+          sortedStr = "".join(sorted(i))
+          if (not sortedStr in hashMap):
+               hashMap[sortedStr] = [i]
+          else:
+               hashMap[sortedStr].append(i)
+          
+     for j in hashMap.values():
+          result.append(j)
+     
+     return result 
+
+
+print(group_anagrams(["eat","tea","tan","ate","nat","bat"]))
