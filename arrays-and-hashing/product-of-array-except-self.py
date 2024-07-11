dc7aa356f37d6530623ed5b1d1677ebccbd97e4d
Vicky Sekhon <114193273+VickySekhon@users.noreply.github.com>
2024-07-11 12:26:18 -0400

    Optimized solution by myself : )

--- arrays-and-hashing/product-of-array-except-self.py
+++ arrays-and-hashing/product-of-array-except-self.py
@@ -10,7 +10,8 @@ def productExceptSelf(nums):
      Returns:
           int[]: The output array where each element is the product of all elements in nums except the corresponding element.
      """
-     # Implementation goes here
+     # TODO: creating left and right arrays and calculating the product using math library Prod()
+     # TODO: PROBLEM: cannot use non-built in libraries in Python
      result = []
      
      for i in range(len(nums)):
@@ -20,4 +21,97 @@ def productExceptSelf(nums):
 
      return result
 
-print(productExceptSelf([-1,0,1,2,3]))
+print("FIRST: ", productExceptSelf([-1,0,1,2,3]))
+
+
+def productExceptSelf2(nums):
+     """
+     Calculates the product of array except self.
+
+     Args:
+          nums (int[]): The input array of integers.
+
+     Returns:
+          int[]: The output array where each element is the product of all elements in nums except the corresponding element.
+     """
+     # TODO: appending a temporary array the different combinations of left and right arrays connected together, then performing multiplication of each array manually and appending the result into the final array
+     # TODO: PROBLEM: O(n^2) time complexity due to nested for loop
+     arr = []
+     for i in range(len(nums)):
+          leftArr = nums[0:i]
+          rightArr = nums[i+1:len(nums)]
+          arr.append(leftArr+rightArr)
+     
+     result = []
+     for i in arr:
+          product = 1
+          for j in i:
+               product *= j
+          result.append(product)
+
+     return result
+
+print("SECOND: ", productExceptSelf2([-1,0,1,2,3]))
+
+def productExceptSelf3(nums):
+     """
+     Calculates the product of array except self.
+
+     Args:
+          nums (int[]): The input array of integers.
+
+     Returns:
+          int[]: The output array where each element is the product of all elements in nums except the corresponding element.
+     """
+     # TODO: calculating total product and then dividing by each index
+     # TODO: PROBLEM: cannot use division
+     totalProd = 1
+     
+     for i in nums:
+          totalProd *= i
+     
+     result = []
+     for i in nums:
+          temp = totalProd
+          if (i != 0):
+               temp //= i
+          else:
+               temp = 0
+          result.append(temp)
+
+     return result
+
+print("THIRD: ", productExceptSelf2([-1,0,1,2,3]))
+
+
+def productExceptSelf4(nums):
+     """
+     Calculates the product of array except self.
+
+     Args:
+          nums (int[]): The input array of integers.
+
+     Returns:
+          int[]: The output array where each element is the product of all elements in nums except the corresponding element.
+     """
+     # Implementation goes here
+     prefix = [1] * len(nums)
+     postfix = [1] * len(nums)
+     
+     # prefix
+     for i in range(1, len(nums)):
+          prefix[i] = prefix[i-1] * nums[i-1]
+          
+     # postfix
+     for i in range(len(nums) - 2, -1, -1):
+          postfix[i] = postfix[i+1] * nums[i+1]
+     
+     result = []
+     ctr = 0
+     while ctr < len(prefix):
+          result.append(postfix[ctr] * prefix[ctr])
+          ctr += 1
+
+     return result
+
+print("**CORRECT**: ", productExceptSelf4([-1,0,1,2,3]))
