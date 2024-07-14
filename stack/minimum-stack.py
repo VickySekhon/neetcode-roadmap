b8b831839bff12ce9f5df09ee30a0483fc985bb8
Vicky Sekhon <114193273+VickySekhon@users.noreply.github.com>
2024-07-14 00:43:03 -0400

    updated

--- stack/minimum-stack.py
+++ stack/minimum-stack.py
@@ -52,3 +52,51 @@ stack.push(2)
 stack.push(7)
 print(stack.top())
 print(stack.getMin())
+
+# TODO: linked node implementation using two stacks
+class LinkedNode:
+     def __init__(self, value, next=None):
+          self.value = value
+          self.next = next
+
+class LinkedStack:
+     def __init__(self, top):
+          self.top = top
+          self.minTop = top
+     
+     def push(self, value):
+          # regular stack
+          newNode = LinkedNode(value, self.top)
+          self.top = newNode
+          
+          # min stack 
+          newMinNode = LinkedNode(min(value, self.minTop.value) if not self.minTop is None else value, self.minTop)
+          self.minTop = newMinNode
+     
+     def pop(self)-> int:
+          # regular stack
+          temp = self.top.value
+          self.top = self.top.next
+          self.top.next = None
+          
+          # min stack
+          self.minTop = self.minTop.next
+          self.minTop.next = None
+          
+          return temp # return the popped value from the actual stack
+
+     def topOf(self)->int:
+          return self.top.value
+     
+     def getMin(self)->int:
+          return self.minTop.value
+          
+          
+node = LinkedNode(2)
+linkStack = LinkedStack(node)
+linkStack.push(-1)
+linkStack.push(2)
+linkStack.push(1)
+linkStack.push(0)
+
+print(linkStack.getMin())
