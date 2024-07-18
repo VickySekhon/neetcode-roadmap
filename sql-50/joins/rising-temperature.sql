7d045124e17848e50233d655e6e3381170ddecc5
Vicky Sekhon <114193273+VickySekhon@users.noreply.github.com>
2024-07-17 21:56:53 -0400

    Start of sql problem solving

---
+++ sql-50/joins/rising-temperature.sql
@@ -0,0 +1,35 @@
+/*
+Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+*/
+
+SELECT day_after.id
+FROM Weather AS day_after
+INNER JOIN Weather AS day_before
+WHERE DATEDIFF(day_after.recordDate, day_before.recordDate)=1
+AND day_after.temperature>day_before.temperature
