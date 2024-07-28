08c074d2aff6c6aa7325a1826dd6cf9f607317c1
Vicky Sekhon <114193273+VickySekhon@users.noreply.github.com>
2024-07-28 01:32:38 -0400

    cool problem, found solution quickly

---
+++ arrays-and-hashing/3-sum-closest.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        result = nums[0] + nums[1] + nums[len(nums) - 1]
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            j = i + 1
+            k = len(nums) - 1
+            while (j < k):
+                sum = nums[i] + nums[j] + nums[k]
+                if (sum < target):
+                    j+=1
+                else:
+                    k-=1
+                
+                result = sum if abs(target - sum) < abs(target - result) else result
+        return result   
+   
+# moving the pointer j up when sum is too small
+# moving the pointer k down when sum is too large
+# can't find the sum, then return the base case (result) because it's guaranteed there are at least 3 elements in list
