07fcdd9d4aed46de56685460c859408e15579f50
Vicky Sekhon <114193273+VickySekhon@users.noreply.github.com>
2025-02-06 23:07:45 -0500

    Finished 3-sum

--- 2-pointers/3-sum.py
+++ 2-pointers/3-sum.py
@@ -49,45 +49,41 @@ print(sol.threeSum(numbers))
 # [-4, -1, -1, 0, 1, 2]
 
 
-
-class Solution2:
+class Solution:
      # O(n^2 log (n))
      def threeSum(self, nums: list[int]) -> list[list[int]]:
         res = []
-        temp = []
-        seen = set()
-        nums.sort() # O(n log (n))
-        print(nums)
-        for i in range(len(nums)):
-             if i < len(nums) - 1 and nums[i] == nums[i+1]:
+        
+        nums.sort()
+        
+        for i, a in enumerate(nums):
+             if a > 0:
+                  break
+             if i > 0 and nums[i] != nums[i-1]:
                   continue
-             a, b = 0, 0
-             indices = self.twoSumSorted(nums, -nums[i], i)
-             if indices:
-                  a, b = indices
-                  temp.append(nums[i])
-                  temp.append(nums[a])
-                  temp.append(nums[b])
-                  temp.sort()
-                  if not tuple(temp) in seen: 
-                      seen.add(tuple(temp))
-                      res.append(temp[:])
-                  temp = []
+             
+             l = i + 1
+             r = len(nums) - 1
+             while l < r:
+                  threeSum = nums[l] + nums[r] + a
+                  if threeSum < a:
+                       l += 1
+                  elif threeSum > a:
+                       r -= 1
+                  else:
+                       res.append([nums[l], nums[r], a])
+                       l+=1
+                       r-=1
+                       while nums[l] == nums[l-1] and l < r:
+                            l += 1
         return res
      
      # O(n)
      def twoSumSorted(self, nums, target, i):
-          l = 0
+          l = i+1
           r = len(nums) - 1
           
           while l < r:
-               if l == i:
-                    l += 1
-                    continue
-               if r == i:
-                    r -= 1
-                    continue
-               
                if nums[l] + nums[r] == target:
                     return l, r
                if nums[l] + nums[r] > target:
@@ -96,7 +92,9 @@ class Solution2:
                     l += 1
           return None
      
-sol2 = Solution2()
-#numbers2 = [-1,0,1,2,-1,-4]
-numbers2 = [-1,0,1,2,-1,-4,-2,-3,3,0,4]
-print(sol2.threeSum(numbers2))
+sol = Solution()
+#numbers = [-1,0,1,2,-1,-4]
+numbers = [-1,0,1,2,-1,-4,-2,-3,3,0,4]
+print(sol.threeSum(numbers))
+# numbers = [-1,0,1,2,-1,-4]
+# [-4, -1, -1, 0, 1, 2]
