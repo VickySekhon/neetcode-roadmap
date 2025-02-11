fa2b96ba1edffc9df6fbfb4261cc6d4bb551aacf
Vicky Sekhon <114193273+VickySekhon@users.noreply.github.com>
2025-02-11 00:30:20 -0500

    241/324 testcases passed, better than before

--- 2-pointers/trapping_rain_water.py
+++ 2-pointers/trapping_rain_water.py
@@ -8,23 +8,31 @@ def trappingRainWater(heights):
           j = i+1
 
           raindrops = []
-          print(i,j)
+          
           while j < length and heights[j] < l:
                raindrops.append(heights[j])
                j += 1
           print(raindrops)
-          if j < length and len(raindrops) > 0:
+          
+          if j >= length:
+               largest = max(raindrops)
+               largest_i = raindrops.index(largest)
+               t = 0
+               while t < largest_i:
+                    total += min(l, largest) - raindrops[t]
+                    t+=1
+          elif j < length and len(raindrops) > 0:
                r = heights[j]
                for drop in raindrops:
                     total += min(l, r) - drop
           else:
                j = i+1
-          print(total)
+          
           raindrops = []
           i = j
      
      return total
 
 #print(trappingRainWater([0,2,0,3,1,0,1,3,2,1]))
-#print(trappingRainWater([0,1,0,2,1,0,1,3,2,1,2,1]))
+print(trappingRainWater([0,1,0,2,1,0,1,3,2,1,2,1]))
 print(trappingRainWater([4,2,3]))
