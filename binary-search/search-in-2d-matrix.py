1e78af6405d4ee6c5e7153ec8c82b2865ebb7545
Vicky Sekhon <114193273+VickySekhon@users.noreply.github.com>
2026-02-01 00:42:52 -0500

    working on 2-d matrix search, almost there

---
+++ binary-search/search-in-2d-matrix.py
@@ -0,0 +1,39 @@
+
+""" 
+Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 10
+
+Output: true
+"""
+
+def searchMatrix(matrix: list[list[int]], target: int) -> bool:
+     low_row, hi_row = 0, len(matrix)-1
+     
+     while low_row <= hi_row:
+          mid_row = (low_row+hi_row)//2
+          
+          low_col, hi_col = 0, len(matrix[mid_row]) - 1
+          mid_col = 0 # Use this as a heuristic that tells us which subarray to search (left or right)
+          while low_col <= hi_col:
+               mid_col = (low_col+hi_col)//2
+               
+               if matrix[mid_row][mid_col] == target:
+                    return True
+               if target > matrix[mid_row][mid_col]:
+                    low_col = mid_col+1
+               else:
+                    hi_col = mid_col-1
+          
+          # Search the right
+          if mid_col > ((len(matrix[mid_row])-1)//2):
+               low_row = mid_row+1
+          else:
+               hi_row = mid_row-
+     
+     return False
+
+# matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]]
+# target = 15
+matrix=[[1],[3]]
+target=3
+
+print(searchMatrix(matrix, target))
