1bae58b55c6665c5603fd0e40fbcc222fc5cb0da
Vicky Sekhon <114193273+VickySekhon@users.noreply.github.com>
2026-02-17 23:03:33 -0500

    modified binary search

--- binary-search/find-minimum-in-rotated-sorted-array.py
+++ binary-search/find-minimum-in-rotated-sorted-array.py
@@ -4,22 +4,22 @@ class Solution:
           return nums[0]
 
 
-     def findMin2(self, nums: list[int]) -> int:
+     def findMin2(self, nums):
           l, r = 0, len(nums)-1
-          if nums[r] < nums[l]: # array is rotated
-               mid = 0
-               while l < r:
-                    mid = (l+r)//2
-                    if nums[l] <= nums[mid]: # min is in the right
-                         l = mid+1
-                    elif nums[mid] < nums[r]: #  min is in the left
-                         r = mid
-          return nums[l]
-     
+          
+          while l < r:
+               mid = (l+r)//2
+               if nums[mid] > nums[r]: # array is shifted to the <--
+                    l = mid + 1
+               else: # array is shifted to the -->
+                    r = mid
+          return nums[r]
+
 sol = Solution()
 # x = sol.findMin2([3,4,5,6,1,2])
-# x = sol.findMin2([4,5,6,7])
-x = sol.findMin2(nums=[3,4,5,1,2])
+# x = sol.findMin2([4,5,6,7]) => [6,7,4,5] => [7,4,5,6]
+#x = sol.findMin2(nums=[4,5,6,7,0,1,2 ])
+x = sol.findMin2([5,1,2,3,4])
 print(x)
 
 
