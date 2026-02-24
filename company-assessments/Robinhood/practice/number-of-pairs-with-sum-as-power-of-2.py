56e77403a40994ba7ba8cb7d01e3c44e3a6f980c
Vicky Sekhon <114193273+VickySekhon@users.noreply.github.com>
2026-02-24 16:07:20 -0500

    combinatorics problem

--- company-assessments/Robinhood/practice/number-of-pairs-with-sum-as-power-of-2.py
+++ company-assessments/Robinhood/practice/number-of-pairs-with-sum-as-power-of-2.py
@@ -1,3 +1,4 @@
+import math
 def pairs_with_power_of_two_sum(nums):
      """ 
      You’re given an array nums.
@@ -24,4 +25,61 @@ def pairs_with_power_of_two_sum(nums):
      (2,3): 3+5 = 8   ✅
      (3,3): 5+5 = 10  ❌
      """
-     return
+     # pairs = 0
+     # for i in range(len(nums)):
+     #      for j in range(i, len(nums)):
+     #           pair_sum = nums[i] + nums[j]
+               
+     #           while pair_sum > 1:
+     #                pair_sum /= 2
+               
+     #           if pair_sum == 1:
+     #                pairs += 1
+     # return pairs
+     
+     freq = {}
+     for num in nums:
+          if freq.get(num):
+               freq[num] += 1
+          else:
+               freq[num] = 1
+     
+     pows = []
+     _max = max(nums) * 2
+     highest_pow = math.floor(math.sqrt(_max))
+     for i in range(highest_pow+1):
+          pows.append(2**i)
+
+     print(freq, pows)
+     pairs = 0
+     for a in freq:
+          for p in pows:
+               b = p-a
+               if b not in freq:
+                    continue
+               if a != b and b>a:
+                    pairs += freq[a]*freq[b]
+               elif a == b:
+                    pairs += int((freq[a]*(freq[a]+1))/2)
+                    
+     return pairs
+x = pairs_with_power_of_two_sum([1,1,3,5])
+print(x)
+
+
+"""
+[1,1,3,8,8]
+
+8 + 8 = 16
+
+{1: 2, 3: 1, 8: 2}
+pows = 1,2,4,8,16
+
+p-a = b
+(1) if b is in map and not equal to a, then if b > a then add it (prevent duplicates)
+(2) if b is in map and equal to a
+
+1-1=0 not in map
+2-1=1 in the map, add pairing to the pairs
+
+"""
